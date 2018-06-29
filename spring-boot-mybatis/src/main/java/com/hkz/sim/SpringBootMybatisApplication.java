a09770470d5adf0d9c982b0fb6ff6f17b9ed364e
huangkz <741386917@qq.com>
2018-06-29 18:17:40 +0800

    [添加模块]springboot-mybatis 无注释

---
+++ spring-boot-mybatis/src/main/java/com/hkz/sim/SpringBootMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.hkz.sim;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.hkz.sim.mapper")
+public class SpringBootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisApplication.class, args);
+	}
+}
